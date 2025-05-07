09c0be83b249045f641ddba2df30931fc17c9589
Haruta Tasaki <hrtune@gmail.com>
2025-05-07 21:39:37 +0900

    Implement reducers

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,5 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const examplePayload = {
+  name: "Aloe Vera",
+  image: "https://cdn.pixabay.com/photo/2018/04/02/07/42/leaf-3283175_1280.jpg",
+  description: "Soothing gel used for skin ailments.",
+  cost: "$14"
+}
+
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
@@ -7,13 +14,30 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload
+
+      // Check if the item already exists in the cart by comparing names
+      const existingItem = state.items.find(item => item.name === name);
+
+      if (existingItem) {
+        // If item already exists in the cart, increase its quantity
+        existingItem.quantity++;
+      } else {
+        // If item does not exist, add it to the cart with quantity 1
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
+
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload)
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload; // Destructure the product name and new quantity from the action payload
+      // Find the item in the cart that matches the given name
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity; // If the item is found, update its quantity to the new value
+      }
     },
   },
 });
